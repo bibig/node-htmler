56bcb0238d4f0119fff710eec3787bcbfe6fb89a
bibig <liushuxun@gmail.com>
2014-03-31 23:51:02 +0800

    kick off

---
+++ README.md
@@ -0,0 +1,33 @@
+# Htmler
+
++ an easy way to render html string.
++ although sorts of template engines are popular. but sometimes we also need the tranditional way to render html.
+
+## Install
+ npm install htmler
+
+## Version
+  0.0.1
+
+## Usage
+  ```javascript
+    var htmler = require('htmler');
+    var img = htmler.img;
+    
+    
+    img({src: 'http://www.example.com/xxx.png'});
+    // -> <img src="http://www.example.com/xxx.png">
+    
+    var div = htmler.div;
+    div({id: 'myDiv', class: 'myClass'}).html('hello world');
+    // -> <div id="myDiv" class="myClass">hello world</div>
+  
+    var ul = htmler.ul;
+    var li = htmler.li;
+    ul('myStyle').html(
+      li().html('hello'),
+      li('active').html('htmler')
+    );
+    // -> <ul class="myStyle"><li>hello</li><li class="active">htmler</li></ul>
+  
+  ```
---
+++ libs/htmler.js
@@ -0,0 +1,117 @@
+var Html = exports = module.exports = {};
+
+function registerTag (name, hasText) {
+    if (hasText) {
+        Html[name] = tag(name);
+    } else {
+        Html[name] = singleTag(name);
+    }
+}
+
+function tag (name) {
+    return function (properties) {
+        return {
+            text: '',
+            name: name,
+            properties: convertProperties(properties),
+            contain: contain,
+            html: contain
+        };
+    }
+}
+
+function singleTag (name) {
+    return function (properties) {
+        var props = makeProperties(convertProperties(properties));
+        return makeTag(name, props, false);
+    }
+}
+
+function convertProperties (properties) {
+    properties = properties || {};
+    
+    if (typeof properties == 'string') {
+        return {class: properties};
+    }
+    
+    if (Array.isArray(properties)) {
+        return {class: properties.join(' ')};
+    }
+    
+    return properties;
+}
+
+function contain () {
+		if (arguments.length == 1 && Array.isArray(arguments[0])) {
+				this.text = arguments[0].join('');
+		} else {
+			for (var i = 0; i < arguments.length; i++) {
+					this.text += arguments[i];
+			}
+		}
+    return render.call(this);
+}
+
+function render () {
+    var props = makeProperties(this.properties);
+    return makeTag(this.name, props, this.text);
+}
+
+
+
+function makeTag(name, props, html) {
+    var props = props.length > 0 ? ' ' + props.join(' ') : '';
+    var beginTag = surround(name + props, '<', '>');
+    var endTag;
+    if (html === false) {
+        return beginTag;
+    } else {
+        endTag = surround('/' + name, '<', '>');
+        return surround(html, beginTag, endTag);
+    }
+}
+
+function makeProperties (properties) {
+    var props = [];
+    var keys = Object.keys(properties);
+    
+    keys.forEach(function (key) {
+        props.push(makeProperty(key, properties[key]));
+    });
+    
+    return props;
+}
+
+function makeProperty(name, value) {
+    
+    if (value === '' || value === undefined || value === null) {
+        return '';
+    }
+    
+    if (Array.isArray(value)) {
+        value = value.join(' ');
+    }
+    return [name, surround(value)].join('=');
+}
+
+
+function surround (str/*sign*/) {
+    var startSign = '"', endSign = '"';
+    
+    if (arguments.length == 2) {
+        startSign = endSign = arguments[1];
+    } else if (arguments.length == 3) {
+        startSign = arguments[1]
+        endSign = arguments[2];
+    }
+    
+    return startSign + str + endSign;
+}
+
+['html', 'head', 'body', 'style', 'title', 'div','p','span','label','h1','h2','h3', 'h4', 'h5', 'h6', 'table','caption', 'thead', 'tbody', 'th','td','tr','li','ul','ol','dl','dt','dd','center','strong','b','em','sup','sub','a', 'textarea', 'button', 'select', 'option', 'optgroup', 'form', 'fieldset', 'legend', 'address', 'section', 'script'].forEach(function (name) {
+    registerTag(name, true);
+});
+
+['img', 'input', 'link', 'meta'].forEach(function (name) {
+    registerTag(name, false);
+});
